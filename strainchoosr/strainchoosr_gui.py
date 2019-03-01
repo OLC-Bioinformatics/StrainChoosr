55e698dc2bb51371fa2c95b53c70b298eb2245bb
lowandrew <andrew.low@canada.ca>
2019-03-01 14:52:41 -0500

    Gui now lets you choose color for selected strains on tree

--- strainchoosr/strainchoosr_gui.py
+++ strainchoosr/strainchoosr_gui.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-from PyQt5.QtWidgets import QApplication, QFileDialog, QMainWindow, QPushButton, QErrorMessage, QLabel, QSpinBox
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QFileDialog, QMainWindow, QPushButton, QErrorMessage, QLabel, QSpinBox, QColorDialog
+from PyQt5.QtGui import QPixmap, QPalette, QColor
 from PyQt5.QtCore import Qt
 from strainchoosr import strainchoosr
 import subprocess
@@ -26,27 +26,54 @@ class StrainChoosrGUI(QMainWindow):
             os.makedirs(self.tmpdir)
         self.fasta_button = None
         self.newick_button = None
+        self.newick_tree_label = None
         self.strainchoosr_button = None
         self.strain_number_input = QSpinBox(self)
         self.newick_tree = None
+        self.color_dialog_button = None
+        self.color_label = None
+        self.color_label_palette = None
+        self.color = 'red'
         self.init_ui()
 
     def init_ui(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-        self.fasta_button = QPushButton('Select FASTA files', self)
-        self.fasta_button.move(100, 100)
-        self.fasta_button.resize(200, 40)
-        self.fasta_button.clicked.connect(self.get_fasta_files)
+        # self.fasta_button = QPushButton('Select FASTA files', self)
+        # self.fasta_button.move(100, 100)
+        # self.fasta_button.resize(200, 40)
+        # self.fasta_button.clicked.connect(self.get_fasta_files)
         self.newick_button = QPushButton('Select Newick Tree', self)
         self.newick_button.resize(200, 40)
+        self.newick_tree_label = QLabel(self)
+        self.newick_tree_label.move(210, 10)
+        self.newick_tree_label.resize(100, 20)
+        self.newick_tree_label.setText(str(self.newick_tree))
+        self.newick_tree_label.show()
         self.newick_button.clicked.connect(self.get_newick_tree)
         self.strainchoosr_button = QPushButton('Run StrainChoosr', self)
         self.strainchoosr_button.setEnabled(False)
         self.strainchoosr_button.clicked.connect(self.run_strainchoosr)
         self.strainchoosr_button.resize(200, 40)
         self.strainchoosr_button.move(0, 200)
+        self.color_dialog_button = QPushButton('Choose Strain Color', self)
+        self.color_dialog_button.setEnabled(False)
+        self.color_dialog_button.move(0, 245)
+        self.color_dialog_button.resize(150, 30)
+        self.color_dialog_button.clicked.connect(self.choose_color)
         self.strain_number_input.setEnabled(False)
+        self.color_label = QLabel(self)
+        self.color_label.move(155, 245)
+        self.color_label.setAutoFillBackground(True)
+        self.color_label_palette = self.color_label.palette()
+        self.color_label_palette.setColor(QPalette.Background, QColor(self.color))
+        self.color_label.setPalette(self.color_label_palette)
+
+    def choose_color(self):
+        color = QColorDialog.getColor()
+        self.color = color.name()
+        self.color_label_palette.setColor(QPalette.Background, QColor(self.color))
+        self.color_label.setPalette(self.color_label_palette)
 
     def get_fasta_files(self):
         options = QFileDialog.Options()
@@ -65,12 +92,20 @@ class StrainChoosrGUI(QMainWindow):
                                                  options=options)
         self.newick_tree = file_input[0]
         number_leaves = len(ete3.Tree(self.newick_tree).get_leaves())
-        self.strain_number_input.move(0, 300)
+        strain_number_label = QLabel(self)
+        strain_number_label.setText('Select number of strains to choose')
+        strain_number_label.move(110, 150)
+        strain_number_label.resize(300, 20)
+        strain_number_label.show()
+        self.strain_number_input.move(0, 150)
         self.strain_number_input.setMaximum(number_leaves)
         self.strain_number_input.setMinimum(2)
         self.strain_number_input.setEnabled(True)
-        self.strain_number_input.show()
         self.strainchoosr_button.setEnabled(True)
+        self.color_dialog_button.setEnabled(True)
+        self.color_dialog_button.show()
+        self.color_label.show()
+        self.newick_tree_label.setText(os.path.split(self.newick_tree)[1])
 
     def run_strainchoosr(self):
         if self.newick_tree is None:
@@ -87,13 +122,14 @@ class StrainChoosrGUI(QMainWindow):
             # I couldn't find a way to kill the ete3 PyQt app via the code, so my hacky solution is to run tree rendering via a subprocess
             # so that my GUI doesn't know anything about the ete3 GUI and therefore whatever interaction was occurring
             # can no longer occur.
-            subprocess.call('strainchoosr_drawimage {} {} {}'.format(self.newick_tree, self.strain_number_input.value(), self.tmpdir), shell=True)
+            cmd = 'strainchoosr_drawimage {} {} {} "{}"'.format(self.newick_tree, self.strain_number_input.value(), self.tmpdir, self.color)
+            subprocess.call(cmd, shell=True)
             pic = QLabel(self)
             pixmap = QPixmap(os.path.join(self.tmpdir, 'image.png'))
             scaled_pixmap = pixmap.scaled(400, 400, Qt.KeepAspectRatio, Qt.FastTransformation)
             pic.setPixmap(scaled_pixmap)
             pic.resize(scaled_pixmap.width(), scaled_pixmap.height())
-            pic.move(300, 10)
+            pic.move(400, 10)
             pic.show()
 
     def closeEvent(self, event):
@@ -105,13 +141,14 @@ def draw_image_wrapper():
     tree_file = sys.argv[1]
     num_strains = int(sys.argv[2])
     output_dir = sys.argv[3]
+    color = sys.argv[4]
     tree = ete3.Tree(tree_file)
     diverse_strains = strainchoosr.pd_greedy(tree=tree, number_tips=num_strains, starting_strains=[])
     strainchoosr.create_colored_tree_tip_image(tree_to_draw=tree,
                                                output_file=os.path.join(output_dir, 'image.png'),
                                                representatives=strainchoosr.get_leaf_names_from_nodes(diverse_strains),
                                                mode='r',
-                                               color='red')
+                                               color=color)
 
 
 def main():
